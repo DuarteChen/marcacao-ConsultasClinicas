031dbb1c37e916bea2bbb30a3582374c38fd57b8
Duarte Chen <duartechen@gmail.com>
2024-11-05 15:39:54 +0000

    Consultas só podem ser marcadas das 8 às 20

--- AddServerImpl.java
+++ AddServerImpl.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class AddServerImpl extends UnicastRemoteObject implements AddServerIntf {
 
@@ -40,12 +42,45 @@ public class AddServerImpl extends UnicastRemoteObject implements AddServerIntf 
     return result.length() > 0 ? result.toString() : "Nenhum médico encontrado.";
   }
   
+
+
+  public static boolean isvalidDataHora(String dataHora) {
+      // Definindo o formato esperado
+      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+      dateFormat.setLenient(false); // Não permitir datas inválidas
+
+      try {
+          // Tenta analisar a data
+          Date date = dateFormat.parse(dataHora);
+          
+          // Extrai a hora
+          int hour = date.getHours();
+          // Verifica se a hora está entre 8 (08:00) e 20 (20:00)
+          if (hour < 8 || hour > 20) {
+            System.out.println("Erro: A clinica está fechada às " + hour + " horas.");
+            return false;
+          }
+          System.out.println("A data e a hora são válidas.");
+          return true;
+      } catch (ParseException e) {
+          // Se ocorrer uma exceção, a string não está no formato correto
+          System.out.println( "Erro no formato da data e da hora");
+          return false;
+      }
+  }
+
   @Override
 public String marcarConsulta(int idClient, int idMedico, String dataHora) throws RemoteException {
     String url = "jdbc:mysql://localhost:3306/dbCDProjeto";
     String user = "user";
     String password = "user";
 
+    //as consultas são das 8:00 às 20:00
+    //O formato da string dataHora é "yyyy-mm-dd hh:mm"
+    if (isvalidDataHora(dataHora)) {
+      return "Erro";
+    } //verifica e dá uma mensagem de feedback no terminal
+  
     try (Connection conn = DriverManager.getConnection(url, user, password)) {
         String sql = "INSERT INTO Consulta (data, Cliente_idCliente, Medico_idMedico) " + 
                      "VALUES (?, ?, ?)";
