867fd4543e6bb1c7e90de9953814e2a64e63d610
Duarte Chen <duartechen@gmail.com>
2024-11-05 17:09:04 +0000

    reformating AddClient

--- AddClient.java
+++ AddClient.java
@@ -24,36 +24,44 @@ public class AddClient {
           }
 
 
-    } else if (args[1].trim().equals("listarConsultasDeCliente")){
-        System.out.println("Cliente: " + args[2]);
-        String cliente = args[2];
-        System.out.println("Output: " + addServerIntf.listarConsultas(cliente));
-    } else if (args[1].trim().equals("listarMedicosDe")){
-        System.out.println("Clinica: " + args[2]);
-        String clinica = args[2];
-        System.out.println("Output: " + addServerIntf.listarMedicos(clinica));
-    } else if (args[1].trim().equals("marcarConsulta")) {
-    
-        if (args.length == 8) {
+      } else if (args[1].trim().equals("listarConsultas")){
 
-        int dia = Integer.parseInt(args[2]);
-        int mes = Integer.parseInt(args[3]);
-        int ano = Integer.parseInt(args[4]);
-        int hora = Integer.parseInt(args[5]);
+        if (args.length == 3) {
+          System.out.println("Output: " + addServerIntf.listarConsultas(cliente));
+        }
 
-        int clientID = Integer.parseInt(args[6].trim());
-        
-        int medicID = Integer.parseInt(args[7].trim());
-        System.out.println(" A tentar marcar consulta para o dia: " + dia + "-"+ mes + "-" + ano + " às " + hora + ":00 Para " + clientID + " com médico " + medicID);
-        
-        System.out.println("Output: " + addServerIntf.marcarConsulta(dia, mes , ano, hora, clientID, medicID));
+      } else if (args[1].trim().equals("listarMedicos")){
+
+        if (args.length == 3) {
+          String clinica = args[2];
+          System.out.println("Output: " + addServerIntf.listarMedicos(clinica));
+
         } else {
           System.out.println("Output: Número de argumentos errado");
         }
-        
-    } else {
-      System.out.println("Output: Erro");
-    }
+
+      } else if (args[1].trim().equals("marcarConsulta")) {
+      
+          if (args.length == 8) {
+
+          int dia = Integer.parseInt(args[2]);
+          int mes = Integer.parseInt(args[3]);
+          int ano = Integer.parseInt(args[4]);
+          int hora = Integer.parseInt(args[5]);
+
+          int clientID = Integer.parseInt(args[6].trim());
+          
+          int medicID = Integer.parseInt(args[7].trim());
+          System.out.println(" A tentar marcar consulta para o dia: " + dia + "-"+ mes + "-" + ano + " às " + hora + ":00 Para " + clientID + " com médico " + medicID);
+          
+          System.out.println("Output: " + addServerIntf.marcarConsulta(dia, mes , ano, hora, clientID, medicID));
+          } else {
+            System.out.println("Output: Número de argumentos errado");
+          }
+          
+      } else {
+        System.out.println("Output: Erro nos argumentos\n");
+      }
     } catch(Exception e) {
       System.out.println("Exception: " + e);
     }
